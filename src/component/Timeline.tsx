85129674b39cbab68d1f31cb855462da354cd5b1
ChiamakaRita <rchiamaka331@gmail.com>
2022-11-13 21:20:02 +0100

    Location Timeline for Truck Managaer

---
+++ src/component/Timeline.tsx
@@ -0,0 +1,62 @@
+import timelineData from "../data/timelineData";
+import { BiCurrentLocation } from "react-icons/bi";
+import {AiOutlineArrowLeft} from "react-icons/ai"
+import { BsFillSkipBackwardFill } from "react-icons/bs";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css"
+
+
+export const Timeline = () => {
+
+  const handleBackward = () => {
+    console.log("clicked")
+  }
+
+  return (
+    <div>
+      
+        <BsFillSkipBackwardFill
+          className="back-arrow arrow"
+          onClick={handleBackward}
+        />
+    
+      <h1 className="title">Timeline</h1>
+      <VerticalTimeline>
+        {timelineData
+          .sort((a, b) => {
+            const dateA = a.date_added;
+            const dateB = b.date_added;
+            if (dateA < dateB) return 1;
+            else if (dateA > dateB) return -1;
+            return 0;
+          })
+          .map((timeline) => {
+            return (
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                key={timeline.id}
+                contentArrowStyle={{
+                  borderRight: "7px solid  rgb(0, 0, 0)",
+                }}
+                date={timeline.date_added}
+                dateClassName="date"
+                iconStyle={{ background: "rgb(179, 147, 208)", color: "#fff" }}
+                icon={<BiCurrentLocation />}
+              >
+                <h2 className="vertical-timeline-element-title">
+                  Location: {timeline.current_location}
+                </h2>
+                <p className="vertical-timeline-element-subtitle">
+                  Distance Left : {timeline.rem_distance} km
+                </p>
+              </VerticalTimelineElement>
+            );
+          })}
+      </VerticalTimeline>
+    </div>
+  );
+}
+
---
+++ src/data/timelineData.ts
@@ -0,0 +1,30 @@
+const TimelineData = [
+  {
+    id: "60BFADBA-7260-ED11-94A2-9B20AC60C304",
+    current_location: "Ilorin",
+    rem_distance: 770.0,
+    date_added: "2022-11-09 21:09:00.3033333",
+  },
+  {
+    id: "1DDE0054-CE61-ED11-94A3-FDF17F9B64A3",
+    current_location: "Lokoja",
+    rem_distance: 577.0,
+    date_added: "2022-11-11 14:37:12.8766667",
+  },
+//   {
+//     id: "1DDE0754-CE61-ED11-94A3-FDF17F9B64A3",
+//     current_location: "Abuja",
+//     rem_distance: 567.0,
+//     date_added: "2022-11-10 14:37:12.8766667",
+//   },
+//   {
+//     id: "1DDE0754-CE61-ED11-94A3-FDt17F9B64A3",
+//     current_location: "Lagos",
+//     rem_distance: 567.0,
+//     date_added: "2022-11-13 14:37:12.8766667",
+//   },
+];
+
+
+
+export default TimelineData;
